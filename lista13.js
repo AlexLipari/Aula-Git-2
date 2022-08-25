dc8e5f2c8508628a998b180f5857c006f230025d
Alexandre Lipari <santuslipari@gmail.com>
2022-08-25 18:06:38 -0300

    Algoritimo, criar nomes e senhas no Array

---
+++ lista13.js
@@ -0,0 +1,13 @@
+var nomes = []
+var senhas = []
+var continuar = 1
+var contador = 0
+while (continuar == 1) {
+    nomes[contador] = prompt("insira um nome:")
+    senhas[i] = prompt("insira uma senha com 8 digitos")
+    contador++
+    continuar = parseInt(prompt("Deseja continuar? (1) SIM (2) NÃO"))
+}
+console.log(nomes)
+console.log(senhas)
+
